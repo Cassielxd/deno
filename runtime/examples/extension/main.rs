ac3007699c3be34781a2991883c36947917787dc
李兴栋 <lixingdong@aliyun.com>
2024-09-03 17:59:11 +0800

    ....

--- runtime/examples/extension/main.rs
+++ runtime/examples/extension/main.rs
@@ -10,13 +10,13 @@ use deno_core::error::AnyError;
 use deno_core::op2;
 use deno_core::FsModuleLoader;
 use deno_core::ModuleSpecifier;
-use deno_runtime::deno_permissions::PermissionsContainer;
-use deno_runtime::worker::MainWorker;
-use deno_runtime::worker::WorkerOptions;
+use deno_runtime_tauri::deno_permissions::PermissionsContainer;
+use deno_runtime_tauri::worker::MainWorker;
+use deno_runtime_tauri::worker::WorkerOptions;
 
 #[op2(fast)]
 fn op_hello(#[string] text: &str) {
-  println!("Hello {} from an op!", text);
+    println!("Hello {} from an op!", text);
 }
 
 deno_core::extension!(
@@ -28,20 +28,20 @@ deno_core::extension!(
 
 #[tokio::main]
 async fn main() -> Result<(), AnyError> {
-  let js_path =
-    Path::new(env!("CARGO_MANIFEST_DIR")).join("examples/extension/main.js");
-  let main_module = ModuleSpecifier::from_file_path(js_path).unwrap();
-  eprintln!("Running {main_module}...");
-  let mut worker = MainWorker::bootstrap_from_options(
-    main_module.clone(),
-    PermissionsContainer::allow_all(),
-    WorkerOptions {
-      module_loader: Rc::new(FsModuleLoader),
-      extensions: vec![hello_runtime::init_ops_and_esm()],
-      ..Default::default()
-    },
-  );
-  worker.execute_main_module(&main_module).await?;
-  worker.run_event_loop(false).await?;
-  Ok(())
+    let js_path =
+        Path::new(env!("CARGO_MANIFEST_DIR")).join("examples/extension/main.js");
+    let main_module = ModuleSpecifier::from_file_path(js_path).unwrap();
+    eprintln!("Running {main_module}...");
+    let mut worker = MainWorker::bootstrap_from_options(
+        main_module.clone(),
+        PermissionsContainer::allow_all(),
+        WorkerOptions {
+            module_loader: Rc::new(FsModuleLoader),
+            extensions: vec![hello_runtime::init_ops_and_esm()],
+            ..Default::default()
+        },
+    );
+    worker.execute_main_module(&main_module).await?;
+    worker.run_event_loop(false).await?;
+    Ok(())
 }
